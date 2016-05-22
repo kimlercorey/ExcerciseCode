ef55a777f80901c233240e00c29879bef7a6feed
kimler <kimler@gmail.com>
2016-05-21 20:47:07 -0700

    Create version 1 of code to be used in referenceand deployed for example

---
+++ version1/inc/data.php
@@ -0,0 +1,7 @@
+<?php
+
+// This is the original data provided to include for processing
+$grant_funding = [["UCSD", "Find More Brains", "1000"], ["UCSD", "Drink More Water", "130"], ["Harvard", "Find More Brains", "2000.35"], ["Harvard", "Drink More Water", "30"]];
+$num_rows[] = 4;
+
+?>
