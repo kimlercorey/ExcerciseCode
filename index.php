62756c4d3c8a97cfc1aca72ec65c1bb388c3ec46
kimler <kimler@gmail.com>
2016-05-21 20:32:05 -0700

    Add first draft of standard php approach

---
+++ index.php
@@ -0,0 +1,40 @@
+<?php
+/*
+@title:  Code Excercise
+@author: Kimler (KC) Corey
+
+Assume you have an array of arrays called grant_funding such that:
+
+●	grant_funding[r] contains row r (starting from 0)
+●	grant_funding[r][0] contains a unique grantee name (a string, such as “Harvard”)
+●	grant_funding[r][1] contains a unique program name (a string, such as “Find More Brains”)
+●	grant_funding[r][2] contains a funding amount (a string, such as “2000.35”)
+
+If arrays in your chosen language do not have a member/function to determine the total number of rows, assume it is provided in num_rows. For the purpose of this exercise you can hard code your input in the source file. 
+
+Write a function that displays grant_funding in a simple HTML table, sorted by grantee name, with an extra row for displaying the total funding by program.
+
+* * *
+
+Program has been broken up into subcomponents focused on performing descrete functions
+
+inc/data.php - includes the example data input
+inc/dataReader.php - reads the data into the pre report data
+inc/displayReport.php - function which creates a simple html table sorted by grantee name 
+inc/htmlgen.php - just html building features 
+
+*/
+
+require 'inc/data.php';
+require 'inc/dataReader.php';
+require 'inc/displayReport.php';
+
+// Used to store data reader results
+$results[] = array();
+
+// Read grant_funding data into results
+dataReader($grant_funding, $results);
+
+// Display results
+displayReport($results);
+?>
